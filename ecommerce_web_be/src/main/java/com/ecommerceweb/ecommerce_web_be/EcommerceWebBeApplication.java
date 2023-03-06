1249f176e1b0bfe0be83083741a34bac24884034
diepAu13122001 <91660142+diepAu13122001@users.noreply.github.com>
2023-03-06 23:34:39 +0700

    first commit

---
+++ ecommerce_web_be/src/main/java/com/ecommerceweb/ecommerce_web_be/EcommerceWebBeApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerceweb.ecommerce_web_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceWebBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceWebBeApplication.class, args);
+	}
+
+}
---
+++ ecommerce_web_be/src/test/java/com/ecommerceweb/ecommerce_web_be/EcommerceWebBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerceweb.ecommerce_web_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceWebBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
